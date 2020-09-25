12b34e2d73be0988b4069243cfa1721e564f7e93
Technote <technote.space@gmail.com>
2020-09-26 04:49:51 +0900

    chore: update wp version

--- autoload.php
+++ autoload.php
@@ -4,7 +4,7 @@
  * Plugin URI: https://github.com/wp-content-framework/0-framework-test
  * Description: Test plugin for WP Content Framework
  * Author: Technote
- * Version: 0.1.13
+ * Version: 0.1.14
  * Author URI: https://technote.space
  * Text Domain: framework-test
  * Domain Path: /languages/
